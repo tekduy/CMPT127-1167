81f30ad50867b7439e6dc813d625c6bf706634ae
dduy <dduy@asb9838nu-d07.csil.sfu.ca>
2016-09-14 13:58:25 -0700

    t3 submission

---
+++ 1/t3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+ 
+int main( void )
+{
+  int i = 0;
+ 
+  printf("Enter an integer: ");
+ 
+  scanf( "%d", &i );
+  printf( " %d %d %d\n", i, i, i+1 );
+ 
+  return 0;
+}
