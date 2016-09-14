17b2d674b8bb5c9e3a17ae7b49c3c2cff9af1b4e
dduy <dduy@asb9838nu-d07.csil.sfu.ca>
2016-09-14 14:41:38 -0700

    t4 submission

---
+++ 1/t4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+ 
+int main( void )
+{
+  int a = 0;
+  float i = 0;
+ 
+  printf("Enter an integer: ");
+ 
+  scanf( "%f", &i );
+if ( i > a )
+{
+  int x = floor(i);
+  int y = round(i);
+  int z = ceil(i);
+
+  printf( " %d %d %d\n", x, y, z );
+  return 0;
+}
+else
+{
+  int i = 0;
+  printf( "scanf error: (%d)\n ", i );
+}
+}
+
