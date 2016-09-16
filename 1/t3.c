6ed54208e66e316aebd865f036f794ec541ba7d5
dduy <dduy@asb9838nu-b10.csil.sfu.ca>
2016-09-16 09:15:25 -0700

    t3 version 3

--- 1/t3.c
+++ 1/t3.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <math.h>
- 
+
 int main( void )
 {
   float i = 0;
- 
+
   printf("Enter an integer: ");
- 
+
   scanf( "%f", &i );
   int x = floor(i);
   int y = round(i);
   int z = ceil(i);
-  
+
   printf( " %d %d %d\n", x, y, z );
- 
+
   return 0;
 }
