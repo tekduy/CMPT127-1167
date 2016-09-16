5c0f957d9f1d2feb33cd5c828361bf19f583f3d8
dduy <dduy@asb9838nu-c13.csil.sfu.ca>
2016-09-16 10:30:48 -0700

    t3 submission v4

--- 1/t3.c
+++ 1/t3.c
@@ -5,14 +5,12 @@ int main( void )
 {
   float i = 0;
 
-  printf("Enter an integer: ");
-
   scanf( "%f", &i );
   int x = floor(i);
   int y = round(i);
   int z = ceil(i);
 
-  printf( " %d %d %d\n", x, y, z );
+  printf( "%d %d %d\n", x, y, z );
 
   return 0;
 }
