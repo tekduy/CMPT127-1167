cc005210eaa0d90ad6bd3c432803f2e06a44c85b
dduy <dduy@asb9838nu-c13.csil.sfu.ca>
2016-09-16 11:15:43 -0700

    t5 submission

---
+++ 1/t5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <math.h>
+
+int main( void )
+{
+  int a = 0;
+  float i = 0;
+  int b = 0;
+
+  int result = scanf( "%f", &i );
+while (a < i > a)
+{
+if ( a < i > a )
+{
+  int x = floor(i);
+  int y = round(i);
+  int z = ceil(i);
+
+  printf("%d %d %d\n", x, y, z );
+
+  int result = scanf( "%f", &i );
+}
+if (i == 0)
+{
+  int b = i;
+  int result = printf("scanf error: (%d)\n", b);
+}
+if ( result == EOF )
+{
+  printf("Done.\n");
+}
+}
+}
