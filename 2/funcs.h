c412898ee22bdbc7386366cee66bcfb528fcc010
dduy <dduy@asb9838nu-d01.csil.sfu.ca>
2016-09-21 10:54:32 -0700

    task1 submit v2

--- 2/funcs.h
+++ 2/funcs.h
@@ -1,19 +1,2 @@
-// returns the larger of the two arguments
-int max( int x, int y)
-{
-  if( x > y )
-  {
-    return x;
-  }
-  return y;
-}
-
-// returns the smaller of the two arguments
-int min( int x, int y)
-{
-  if( x < y )
-  {
-    return x;
-  }
-  return y;
-}
+int max( int x, int y);
+int min( int x, int y);
