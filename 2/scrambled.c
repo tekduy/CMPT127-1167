24559738f25100ded18a69343e2b5981813294ab
dduy <dduy@csil-cpu2.cs.surrey.sfu.ca>
2016-09-22 19:13:37 -0700

    scrambled.c v7

--- 2/scrambled.c
+++ 2/scrambled.c
@@ -1,62 +1,73 @@
-#include <stdlib.h>
 #include <stdio.h>
 
-int i, fix, z;
-int yes = 0;
-int flag1 = 0;
-
-int scrambled( unsigned int a[], unsigned int b[], unsigned int len )
+int scrambled (unsigned int a[], unsigned int b[], unsigned int len)
 {
-  fix = len - 1;
+
   if (len == 0)
     {
-      printf("Loop 1\n");
       return 1;
     }
 
-//  for ( i = 0; i < len; i++)
-//    {
-//      if (a[i] == b[i])
-//        {
-//          printf("Loop 2\n");
-//          flag1 = 1;
-//        }
-//      else
-//        {
-//          flag1 = 0;
-//        }
-//    }
+  int c[len], d[len];
+  int j;
 
-//    for ( i = 0; i < len; i++)
-//      {
-//        if (a[i] == b[fix-i])
-//          {
-//            printf("Loop 3\n");
-//            return 1;
-//          }
-//      }
+  for (j=0;j<len;j++)
+    {
+      c[j] = a[j];
+      d[j] = b[j];
+    }
 
-for (z = 0; z < len; z++)
-  {
-    for (i = 0; i < len; i++)
-      {
-        if (a[z] == b[i])
+  int i, k;
+
+  for (i=0;i<len;i++)
+    {
+      int mina = c[i];
+      int minapos = i;
+
+        for (k=i;k<len;k++)
           {
-            printf("i is: %d.......", i);
-            printf("z is: %d.......", z);
-            printf("These are the same - a[z] is: %d and b[i] is: %d\n", a[z], b[i]);
-            yes+=1;
+            if (c[k] < mina)
+              {
+                mina = c[k];
+                minapos = k;
+              }
+          }
+      c[minapos] = c[i];
+      c[i] = mina;
+    }
+
+    for (i=0;i<len;i++)
+      {
+        int minb = d[i];
+        int minbpos = i;
+
+          for (k=i;k<len;k++)
+            {
+              if (d[k] < minb)
+                {
+                  minb = d[k];
+                  minbpos = k;
+                }
+            }
+        d[minbpos] = d[i];
+        d[i] = minb;
+      }
+
+    int total = 0;
+
+    for (i=0;i<len;i++)
+      {
+        if (c[i]==d[i])
+          {
+            total = total + 1;
           }
       }
-  }
-yes = yes/len;
-if (2 > yes)
-{
-if (0 < yes)
-  {
-    printf("Yes\n");
-    return 1;
-  }
-}
-  return 0;
+    if (total == len)
+      {
+        return 1;
+      }
+    else
+      {
+        return 0;
+      }
 }
