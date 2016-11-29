ac55bd0d88402f5c14543f587bb7b8908f423173
dduy <dduy@csil-cpu2.cs.surrey.sfu.ca>
2016-11-29 01:31:42 -0800

    Task 1 sub

--- 6/t1.c
+++ 6/t1.c
@@ -4,24 +4,6 @@
 #include <assert.h>
 #include "intarr.h"
 
-/*
-typedef struct {
-  int* data;
-  unsigned int len;
-} intarr_t;
-*/
-
-/* A type for returning status codes */
-/*
-typedef enum {
-  INTARR_OK,
-  INTARR_BADARRAY,
-  INTARR_BADINDEX,
-  INTARR_BADALLOC,
-  INTARR_NOTFOUND
-} intarr_result_t;
-*/
-
 /* LAB 6 TASK 1 */
 
 /*
@@ -32,42 +14,22 @@ typedef enum {
 */
 int intarr_save_binary( intarr_t* ia, const char* filename ){
 
-/*
-const size_t len = ia->len;
-intarr_t arr[len];
+assert(ia);
+assert(ia->data);
+assert(ia->len);
 
-intarr_t * arr = malloc(sizeof(intarr_t));
-
-if( arr == 0 )
-{
-	printf( "Failed to allocate memory for intarr\n" );
-	return -1;
+FILE* f = fopen("filename", "w");
+if (f == NULL){
+  puts ("Failed to open image file for writing");
+	return 1; //Unable to open file
 }
 
-arr->data = malloc(len * sizeof(int));
-assert (arr->data != NULL);
-arr->len = len;
-
-if (ia == NULL){
-	return INTARR_BADARRAY;
-}
-if(ia != NULL){
-	arr->data = ia->data;
-}
-else{
-	return INTARR_BADINDEX;
-}
-*/
-
+//Write
 unsigned int len = ia->len;
 intarr_t * arr = intarr_create(len);
 arr->len = ia->len;
 arr->data = ia->data;
 
-FILE* f = fopen("filename", "w");
-if (!f){
-	return 1; //Unable to open file
-}
 fwrite(arr, sizeof(intarr_t), len, f);
 fclose(f);
 
@@ -80,44 +42,17 @@ return 0;
   newly-allocated intarr_t on success, or NULL on failure.
 */
 intarr_t* intarr_load_binary( const char* filename ){
+  unsigned int len;
 	FILE * f = fopen("filename", "r");
-	if (!f){
+	if (f == NULL){
 		return 1; //Unable to open file
 	}
-	fread(arr, sizeof(intarr_t), len, f);
-	intarr_t * newia = intarr_create(len);
-	newia->len = len;
-	assert(newia);
-	if (f != NULL){
-		while(!feof(f)){
-			fscanf(f, "%d", newia->data);
-		}
-	}
-	else{
-		return NULL;
-	}
+
+  fseek(f, 0, SEEK_END);
+  len = ftell(f);
+  fseek(f, 0, SEEK_SET);
+  intarr_t * newia = intarr_create(len);
+  fread(newia, sizeof(intarr_t), len, f);
 	fclose(f);
 	return newia;
 }
-
-
-	/*
-	const size_t len = 0;
-	FILE* f = fopen("filename", "r");
-	fscanf (f, "%d", len);
-	intarr_t * newia = malloc(sizeof(intarr_t));
-	assert (newia);
-	newia->data = malloc(len*sizeof(int));
-	assert(newia->data);
-	newia->len = len;
-	if (f != NULL){
-		while(!feof(f)){
-			fscanf(f, "%d", newia->data);
-		}
-	}
-	else{
-		return NULL;
-	}
-	fclose(f);
-	return newia;
-}*/
